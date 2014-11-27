bd05701b11dbe03f403cb68ae1431dfb465a819f
thomas-voss <thomas.voss@canonical.com>
2014-11-27 17:38:44 +0100

    Move unref to new line.

--- src/core/dbus/bus.cpp
+++ src/core/dbus/bus.cpp
@@ -278,7 +278,8 @@ std::shared_ptr<Message> Bus::send_with_reply_and_block_for_at_most(
     if (!result)
         throw std::runtime_error(se.print());
 
-    auto reply = Message::from_raw_message(result); dbus_message_unref(result);
+    auto reply = Message::from_raw_message(result);
+    dbus_message_unref(result);
     return reply;
 }
 
